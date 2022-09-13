e51d02bb1ec06ccd9709631a320656202df63c2b
Fahad <76650975+mediocre9@users.noreply.github.com>
2022-09-13 20:17:26 +0500

    optimized!

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Memestan());
+  runApp(const Memestan());
 }
 
 class Memestan extends StatelessWidget {
+  const Memestan({Key ? key}) : super(key : key);
+  
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Center(child: Text('Welcome to mem')),
       ),
