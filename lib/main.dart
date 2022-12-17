e7248e253a943b819fa76096164b5b2a85999144
Fahad Zia <76650975+mediocre9@users.noreply.github.com>
2022-12-17 19:38:36 +0500

    feat: add `drawer`

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: VideoGames(),
+    MaterialApp(
+      home: const VideoGames(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
     ),
   );
 }
 
-// PODO class.....
+// PODO classes.....
 class Game {
   final String name;
   final int rating;
   Game({required this.name, required this.rating});
+}
+
+class DrawerItems {
+  final String label;
+  final IconData icon;
+
+  DrawerItems({required this.label, required this.icon});
 }
 
 class VideoGames extends StatelessWidget {
@@ -30,12 +38,46 @@ class VideoGames extends StatelessWidget {
     Game(name: "Tropico", rating: 10),
   ];
 
+  static final List<DrawerItems> _listOfDrawerItems = [
+    DrawerItems(label: "Home", icon: Icons.home),
+    DrawerItems(label: "Settings", icon: Icons.settings),
+    DrawerItems(label: "About", icon: Icons.person),
+  ];
+
   const VideoGames({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Video Games")),
+      appBar: AppBar(
+        title: const Text("Video Games"),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Center(
+                child: Text(
+                  "Noob Coders",
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: _listOfDrawerItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_listOfDrawerItems[index].label),
+                  leading: Icon(_listOfDrawerItems[index].icon),
+                  onTap: () {},
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: ListView.separated(
